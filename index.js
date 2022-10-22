e29706b8828beecf8edf835455062a954fe0ab76
Fanessa Hilaire <fan@Fanessas-MacBook-Air.local>
2022-10-22 12:14:20 -0400

    update

--- index.js
+++ index.js
@@ -1,20 +1,25 @@
-import express from "express"
-import people from './data.js'
-const PORT = 3033
+import express from "express" // pulling express from node_modules
+// calling library from npm i express 
 
-const app = express()
-app.use(express.json())
+import people from './data.js' // import people from data file (just to test our api)
+const PORT = 3033 // declared port 3000 -4000 only local in our computer 
+
+const app = express() // calls express the function express () and 
+// put new express application inside the app variable 
+
+app.use(express.json()) // * when we get data in were getting in json format
+// line 15 wont work if this is left out , read request in json 
 
 
 app.get('/people',(req,res) => {
 res.send(people)
-})
+}) // getting information from data file , routing method 
 
 
 app.post('/people', (req,res) => {
     const newperson = req.body
     people.push(newperson)
     res.send(people)
-})
+}) // different routing method 
 
 app.listen(PORT,() => console.log( `Listening to http://localhost:${PORT}...`))
